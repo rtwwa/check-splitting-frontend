cafdc44a2dab182dc39ec0cd8ecb0df479368cfa
rtwwa <stalerjoy@gmail.com>
2025-04-11 16:16:39 +0400

    add photo upload button, also install axios

--- src/components/Camera.jsx
+++ src/components/Camera.jsx
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 const Camera = () => {
   const [photo, setPhoto] = useState(null);
 
-  const handleCapture = (e) => {
+  const handleInput = (e) => {
     const file = e.target.files[0];
     if (file) {
       setPhoto(URL.createObjectURL(file));
@@ -11,17 +11,37 @@ const Camera = () => {
   };
 
   return (
-    <div className="flex flex-col items-center gap-4 p-4 bg-white shadow-md rounded-xl w-full max-w-md mx-auto">
-      <label className="cursor-pointer flex flex-col items-center justify-center border-2 border-dashed border-gray-300 rounded-xl w-full h-48 hover:border-blue-500 transition">
+    <div className="flex flex-col items-center gap-4 p-4 bg-white shadow-md rounded-xl w-full max-w-md mx-auto text-gray-500">
+      <label
+        className="
+      cursor-pointer flex flex-col
+      items-center justify-center
+      border-2 border-dashed 
+      border-gray-300 rounded-xl 
+      w-full h-48 transition duration-300
+      hover:border-blue-500
+      hover:text-blue-500"
+      >
         <input
           type="file"
           accept="image/*"
           capture="environment"
-          onChange={handleCapture}
+          onChange={handleInput}
           className="hidden"
         />
-        <span className="text-gray-500">Сделайте фото чека</span>
+        <span>Сделайте фото чека</span>
       </label>
+
+      <div>
+        <button
+          tabIndex={0}
+          type="button"
+          className="hover:text-blue-500 duration-300 transition-colors cursor-pointer"
+          onClick={() => document.querySelector('input[type="file"]').click()}
+        >
+          <span>Или загрузите фото с устройства</span>
+        </button>
+      </div>
 
       {photo && (
         <img
